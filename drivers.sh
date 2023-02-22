a677f8a9fc05ce201dd06535a35dd29cdf2e8185
Heitor Avila <heitoravila99@gmail.com>
2023-02-22 16:04:17 -0300

    experimento 1

--- drivers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt ugprade -y
-sudo apt install -y build-essential make git gcc nvidia-driver-525
-cd ~
-git clone https://github.com/RinCat/RTL88x2BU-Linux-Driver RTL88x2BU
-cd RTL88x2BU
-make
-sudo make install
--- exp_1/max_way_1Kb.sh
+++ exp_1/max_way_1Kb.sh
@@ -1,16 +1,23 @@
-# Experimento 1 - Memória 1Kb
-# nsets * 16 * 1 = 1024 
+#!/bin/bash
+
+nsets=1
+bsize=16
+assoc=64
+repl=r
+
 # cache de dados níveis 1 e 2
-dados1="-cache:dl1 dl1:64:16:1:r"
+dados1="-cache:dl1 dl1:$nsets:$bsize:$assoc:$repl"
 dados2="-cache:dl2 none"
+
 # cache de instruções níveis 1 e 2
-instr1="-cache:il1 il1:64:16:1:r"
+
+instr1="-cache:il1 il1:$nsets:$bsize:$assoc:$repl"
 instr2="-cache:il2 none"
+
 # TLB de dados e instruções (ausentes)
 tlb1="-tlb:dtlb none"
 tlb2="-tlb:itlb none"
-# <name>:<nsets>:<bsize>:<assoc>:<repl>
-# ./sim-cache $dados1 $dados2 $instr1 $instr2 $tlb1 $tlb2
+
 # GCC_1:
 ./sim-cache $dados1 $dados2 $instr1 $instr2 $tlb1 $tlb2 cc1.ss gcc.i
 # GCC_2:
