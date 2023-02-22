c5f4d02dca0b4bd60e2f37f00111a0d77ce7a542
Heitor Avila <heitoravila99@gmail.com>
2023-02-22 18:54:23 -0300

    exp 1 com 3 chips

--- exp_1/4_way_0.5Kb.sh
+++ exp_1/4_way_0.5Kb.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # cache de dados níveis 1 e 2
-dados1="-cache:dl1 dl1:64:16:2:r"
+dados1="-cache:dl1 dl1:8:16:4:r"
 dados2="-cache:dl2 none"
 
 # cache de instruções níveis 1 e 2
 
-instr1="-cache:il1 il1:64:16:2:r"
+instr1="-cache:il1 il1:8:16:4:r"
 instr2="-cache:il2 none"
 
 # TLB de dados e instruções (ausentes)
