eff6f1bc3470c75511125d7714e0fdde3f6ca6a2
Heitor Avila <heitoravila99@gmail.com>
2023-02-22 17:44:06 -0300

    executado

--- exp_1/alg_exp_1.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-./2_way_1Kb.sh > log 2_way_1Kb
-./2_way_2Kb.sh > log 2_way_2Kb
-./4_way_1Kb.sh > log_4_way_1Kb
-./4_way_2Kb.sh > log_4_way_2Kb
-./direto_1Kb.sh > log_direto_1Kb
-./direto_2Kb.sh > log_direto_2Kb
-./max_way_1Kb.sh > log_max_way_1Kb
-./max_way_2Kb.sh > log_max_way_2Kb
